33608ed56ac4f5159aced5a496fd4d577518d05c
GeorgeSemenov <zhora.sem@yandex.ru>
2020-09-30 16:27:31 +0300

    navigation completle

--- source/blocks/navigation/navigation.js
+++ source/blocks/navigation/navigation.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   var $window = $(window);
   $window.scroll(function () {
-    console.log($window.scrollTop()+" 100vh = "+($window.height()-100));
     if ($window.scrollTop()>=($window.height()-100)){
       $(".navigation").css({"background":"#000000"});
     }
