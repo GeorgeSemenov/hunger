e5fd1648d6f126b30a264b01d29106888b19ff54
George Semenov <zhora.sem@yandex.ru>
2020-09-30 08:19:12 +0300

    work with socials and navigation (black background)

--- source/blocks/navigation/navigation.js
+++ source/blocks/navigation/navigation.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-  $(".navigation__logo")
-  .click(function(){
-
+  var $window = $(window);
+  $window.scroll(function () {
+    console.log($window.scrollTop()+" 100vh = "+($window.height()-100));
+    if ($window.scrollTop()>=($window.height()-100)){
+      $(".navigation").css({"background":"#000000"});
+    }
+    else{
+      $(".navigation").css({"background":"transparent"});
+    }
   })
 })
