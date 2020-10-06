8e8a85022abca0e50c6676faba835ac448cec91e
GeorgeSemenov <zhora.sem@yandex.ru>
2020-10-06 16:11:39 +0300

    make up two screens

--- source/blocks/navigation/navigation.js
+++ source/blocks/navigation/navigation.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
   var $window = $(window);
   $window.scroll(function () {
     if ($window.scrollTop()>=($window.height()-100)){
-      $(".navigation").addClass('navigation_desktop-theme_dark');
+      $(".navigation").addClass('navigation_desktop-theme_dark navigation_desktop-theme_small-logo');
     }
     else{
-      $(".navigation").removeClass('navigation_desktop-theme_dark');
+      $(".navigation").removeClass('navigation_desktop-theme_dark navigation_desktop-theme_small-logo');
     }
   })
 })
