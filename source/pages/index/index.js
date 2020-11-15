99a390bf82e5dfb4119e7b645c59d316c9e9b4ec
George Semenov <zhora.sem@yandex.ru>
2020-11-15 22:22:46 +0300

    make data input behave like data input

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -3,3 +3,4 @@ import '../../JS-components/libs.js';
 import '../../blocks/navigation/navigation.js';
 import '../../blocks/main-screen/main-screen.js';
 import '../../blocks/preloader/preloader.js';
+import '../../blocks/sending-form/sending-form.js';
