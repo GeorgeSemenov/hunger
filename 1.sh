f9b799a0b1a36c5750276b7fc12ee7eb1244c5ca
GeorgeSemenov <zhora.sem@yandex.ru>
2020-12-08 12:08:53 +0300

    d d

--- 1.sh
+++ 1.sh
@@ -2,7 +2,7 @@
 read -p "enter your commit message: " messagues
 echo $messagues
 git add --all
-git commit -m $messagues
+git commit -m "${messagues}"
 git push origin
 # FILE="file where i write.txt"
 # /bin/cat <<EOM >$FILE
