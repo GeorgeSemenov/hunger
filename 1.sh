31c9fc26e688c47abac66ff4aeb16d1e645fd65b
GeorgeSemenov <zhora.sem@yandex.ru>
2020-12-08 12:09:47 +0300

    so it's the end of making comipush file

--- 1.sh
+++ 1.sh
@@ -4,12 +4,3 @@ echo $messagues
 git add --all
 git commit -m "${messagues}"
 git push origin
-# FILE="file where i write.txt"
-# /bin/cat <<EOM >$FILE
-# text1
-# text2 # This comment will be inside of the file.
-# The keyword EOM can be any text, but it must start the line and be alone.
-#  EOM # This will be also inside of the file, see the space in front of EOM.
-# EOM # No comments and spaces around here, or it will not work.
-# text4 
-# EOM
