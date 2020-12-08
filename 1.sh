a10b708ba7b92e8a3971de25050cb0f7a4333ced
GeorgeSemenov <zhora.sem@yandex.ru>
2020-12-08 10:08:09 +0300

    “”

--- 1.sh
+++ 1.sh
@@ -1,9 +1,18 @@
-#!/bin/sh
-read -p "enter your commit message: " messagues
-echo $messagues
-git add --all
-git commit -m $messagues
-git push origin
+!/bin/bash
+# lines that start like this are shell comments
+# read projects current directory with $PWD
+echo “running command from” $PWD
+cd $PWD
+git add .
+echo “Enter commit message: “
+git commit -am “$commitMessage”
+git push
+
+# read -p "enter your commit message: " messagues
+# echo $messagues
+# git add --all
+# git commit -m $messagues
+# git push origin
 # FILE="file where i write.txt"
 # /bin/cat <<EOM >$FILE
 # text1
