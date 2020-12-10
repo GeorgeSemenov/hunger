222d5f47ac6dcf8f976a709b1c79d298abc36ed2
GeorgeSemenov <zhora.sem@yandex.ru>
2020-12-10 15:05:14 +0300

    try [H[3~[3~[3~[3~[3~[i hav tried to wrote carousel example

---
+++ source/JS-components/libs.js
@@ -0,0 +1,1 @@
+import '../vendor/slickCarousel/slick/slick.js';
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -4,3 +4,4 @@ import '../../blocks/navigation/navigation.js';
 import '../../blocks/main-screen/main-screen.js';
 import '../../blocks/preloader/preloader.js';
 import '../../blocks/sending-form/sending-form.js';
+import '../../blocks/carousel/carousel.js';
