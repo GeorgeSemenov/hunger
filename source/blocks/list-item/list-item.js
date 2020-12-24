9fc5df04c78da541385b20e26ef39c0af6c3902b
GeorgeSemenov <zhora.sem@yandex.ru>
2020-12-24 08:43:02 +0300

    make item-list

--- source/blocks/list-item/list-item.js
+++ source/blocks/list-item/list-item.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
-  let circleWidth = 5;
-  let priceWidth = $(".list-item__price").css("width");
-  let dishNameWidth = $(".list-item__dish-name").css("width");
-  let containerWidth = $(".list-item__name-and-price").css("width");
-  let freeSpaceWidth = containerWidth.slice(0,(containerWidth.length - 2)) - priceWidth.slice(0,(priceWidth.length - 2)) - dishNameWidth.slice(0,(dishNameWidth.length - 2));
-
-  $(".list-item__dots").css({'width': (freeSpaceWidth + 'px')});
-
-  console.log(`priceWidth = ${priceWidth} /ndishNameWidth = ${dishNameWidth}/ncontainerWidth = ${containerWidth}`);
-
-  let howManyTimes = Math.floor(freeSpaceWidth/(circleWidth*2));
-  console.log(howManyTimes);
-  //alert($(".dots").css('width'));
-  //for (let i = 0; i < 3; i++)
-  for (let i = 0; i< howManyTimes; i++){
-    $(".list-item__dots").append('<div class= "list-item__dot"></div>');
-  }
-  //alert(howManyTimes);
+  setTimeout(function(){setDots()}, 1000);
 })
+
+function setDots() {
+  let circleWidth = 5;
+  $('.list-item').each(function(){
+    let inner = $(this).children('.list-item__name-and-price');
+    let priceWidth = inner.children(".list-item__price").width();
+    let dishNameWidth = inner.children(".list-item__dish-name").width();
+    let containerWidth = inner.width();
+    
+    let freeSpaceWidth = containerWidth - priceWidth - dishNameWidth;
+    inner.children(".list-item__dots").width(freeSpaceWidth);
+    
+    let howManyTimes = Math.floor(freeSpaceWidth/(circleWidth*2));
+    for (let i = 0; i< howManyTimes; i++){
+      inner.children(".list-item__dots").append('<div class= "list-item__dot"></div>');
+    }
+  })
+}
