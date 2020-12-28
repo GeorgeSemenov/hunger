0d1331e66bc76125f4e93b0f9290cba1e18f3c32
George Semenov <zhora.sem@yandex.ru>
2020-12-28 07:29:07 +0300

    menu-list is inapropriate

--- source/blocks/menu/menu.js
+++ source/blocks/menu/menu.js
@@ -1,2 +1,13 @@
 $(document).ready(function(){
+  $(document).ready(function() {
+    $(".menu__tab-container").click(function() {
+      var listsParent = $(this).parent().siblings('.column-right');
+      listsParent.children(".menu__list").hide();
+      var BlockTrigger = $(this).data("trigger");
+      listsParent.children("#menu__list-" + BlockTrigger).show();
+      $(".serv-btn").removeClass("active"); //заберет актив у всех ссылок
+      $(this).addClass("active"); //присвоит нужной
+    });
+  });
+
 })
