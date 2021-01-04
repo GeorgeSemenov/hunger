bd322f12697374bc322f0dd6a7a444c76a18f4a2
George Semenov <zhora.sem@yandex.ru>
2021-01-04 13:54:15 +0300

    menu is ready

--- source/blocks/picture-line/picture-line.js
+++ source/blocks/picture-line/picture-line.js
@@ -1,3 +1,17 @@
+import Swiper from '../../vendor/swiperJS/swiper-bundle.js';
 $(document).ready(function(){
-  var swiper = new Swiper('.picture-line__carousel');
+  var swiper = new Swiper('.picture-line', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints:{
+      510:{
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      1440:{
+        slidesPerView: 4,
+        spaceBetween: 0,
+      }
+    }
+  });
 })
