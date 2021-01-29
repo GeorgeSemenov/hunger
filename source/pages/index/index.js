667f1fa6b5063f5c397c83439743efb55a559a81
GeorgeSemenov <zhora.sem@yandex.ru>
2021-01-29 16:17:10 +0300

    bright idea

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,9 +1,3 @@
-import './index.scss';
-import '../../JS-components/libs.js';
-import '../../blocks/navigation/navigation.js';
-import '../../blocks/main-screen/main-screen.js';
-import '../../blocks/preloader/preloader.js';
-import '../../blocks/sending-form/sending-form.js';
-import '../../blocks/carousel/carousel.js';
-import '../../blocks/menu/menu.js';
-import '../../blocks/picture-line/picture-line.js';
+import pic from '../../img/rings.png'
+import '../../styles/experimental-style.css'
+console.log("hello Dolly");
